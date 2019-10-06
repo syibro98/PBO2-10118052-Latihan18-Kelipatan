7de5766deb609d766c5ae3d175cecc4b9f5289ea
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-10-06 18:42:11 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan18/kelipatan/PBO210118052Latihan18Kelipatan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan18.kelipatan;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Program ini bertujuan untuk mengetahui berapa kelipatan 
+ * selanjutnya
+ * 
+ */
+public class PBO210118052Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(Double i = 3.5; i<=35; i += 3.5) {
+            System.out.println(i);
+        }
+           
+    }
+    
+}
